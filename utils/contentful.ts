a534b2bfdf33e098eadd3f12e22a440108f744d5
Garrett Tolbert <garrett1.tolbert@gmail.com>
2023-02-09 14:42:17 -0600

    update env var

--- utils/contentful.ts
+++ utils/contentful.ts
@@ -2,5 +2,5 @@ import { createClient } from 'contentful';
 
 export const client = createClient({
 	space: process.env.CONTENTFUL_SPACE_ID as string,
-	accessToken: process.env.CONTENTFUL_ACCESS_TOKEN as string,
+	accessToken: process.env.CONTENTFUL_DELIVERY_TOKEN as string,
 });
